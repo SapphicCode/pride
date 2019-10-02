f9f4610c99826a0c8460b4b13733a6a4e9777f00
Cassandra <cassandra@beelen.one>
2019-10-02 16:47:09 +0200

    Run gofmt and various other automated linters

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Pandentia/pride
 
 go 1.13
+
+require golang.org/x/crypto v0.0.0-20191001170739-f9e2070545dc
--- pride/wsize.go
+++ pride/wsize.go
@@ -1,14 +1,14 @@
 package pride
 
 import (
-  "os"
-  "golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/crypto/ssh/terminal"
+	"os"
 )
 
 func windowSize() (height, width int) {
-  width, height, err := terminal.GetSize(int(os.Stdout.Fd()))
-  if err != nil {
-    return 24, 80
-  }
-  return height, width
+	width, height, err := terminal.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		return 24, 80
+	}
+	return height, width
 }
