5ca2fde933c72219fba5d9f1edf1374cdb8aa887
Cassandra <cassandra@beelen.one>
2019-10-02 16:08:25 +0200

    Add trans pride flag (#1)

---
+++ pride/trans.go
@@ -0,0 +1,32 @@
+package pride
+
+import (
+	"fmt"
+)
+
+// PrintTransPrideFlag prints a trans pride flag in your terminal.
+func PrintTransPrideFlag() {
+	height := 24 // TODO: window sizing on unix
+	segmentHeight := height / 5
+	for segment := 1; segment <= 5; segment++ {
+		line := "\n"
+		for i := 0; i < segmentHeight-1; i++ {
+			line += "\n"
+		}
+		if segment == 5 {
+			line += "\n"
+		}
+
+		var colorCode string
+		if segment == 1 || segment == 5 {
+			colorCode = "106"
+		}
+		if segment == 2 || segment == 4 {
+			colorCode = "105"
+		}
+		if segment == 3 {
+			colorCode = "107"
+		}
+		fmt.Printf("\x1B[%sm%s\x1B[0m", colorCode, line)
+	}
+}
