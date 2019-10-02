3670d1b86a12b9f681817bc35d961a0acfbbdbc8
NamedKitten <kitteh@kitteh.pw>
2019-10-02 16:01:08 +0100

    Add function to detect window size (#3)
    * Add function to detect window size
    * Implement window size in the trans pride flag

---
+++ pride/wsize.go
@@ -0,0 +1,14 @@
+package pride
+
+import (
+	"golang.org/x/crypto/ssh/terminal"
+	"os"
+)
+
+func windowSize() (width, height int) {
+	width, height, err := terminal.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		return 80, 24
+	}
+	return
+}
