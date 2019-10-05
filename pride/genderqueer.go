aba699daf73a38904cafeac13f8e964e33f71dc1
Mads T. Kristiansen <madstk1@gmail.com>
2019-10-05 23:30:53 +0200

    Added genderqueer-flag

---
+++ pride/genderqueer.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+// PrintGenderqueerPrideFlag prints a trans pride flag in your terminal.
+func PrintGenderqueerPrideFlag() {
+	_, height := windowSize()
+	segmentHeight := height / 3
+	for segment := 1; segment <= 3; segment++ {
+		line := "\n"
+		for i := 0; i < segmentHeight-1; i++ {
+			line += "\n"
+		}
+		if segment == 3 {
+			line += "\n"
+		}
+
+		var colorCode int
+		switch segment {
+			case 1:		colorCode = 105
+			case 2:		colorCode = 107
+			case 3:		colorCode = 42
+		}
+		fmt.Printf("\x1B[%dm%s\x1B[0m", colorCode, line)
+	}
+}
