9b666cd10645e3de8a6c61535613ba4e3fdb3070
Cassandra <cassandra@beelen.one>
2019-10-07 16:45:18 +0200

    Run gofmt and fix documentation slightly

--- pride/pansexual.go
+++ pride/pansexual.go
@@ -1,32 +1,32 @@
 package pride
 
 import (
-        "fmt"
+	"fmt"
 )
 
-// PrintPansexualPrideFlag prints a Enby pride flag in your terminal.
+// PrintPansexualPrideFlag prints a pansexual pride flag in your terminal.
 func PrintPansexualPrideFlag() {
-        _, height := windowSize()
-        segmentHeight := height / 3
-        for segment := 1; segment <= 3; segment++ {
-                line := "\n"
-                for i := 0; i < segmentHeight-1; i++ {
-                        line += "\n"
-                }
-                if segment == 3 {
-                        line += "\n"
-                }
+	_, height := windowSize()
+	segmentHeight := height / 3
+	for segment := 1; segment <= 3; segment++ {
+		line := "\n"
+		for i := 0; i < segmentHeight-1; i++ {
+			line += "\n"
+		}
+		if segment == 3 {
+			line += "\n"
+		}
 
-                var colorCode string
-                if segment == 1 {
-                        colorCode = "197"
-                }
-                if segment == 2 {
-                        colorCode = "220"
-                }
-                if segment == 3 {
-                        colorCode = "038"
-                }
-                fmt.Printf("\x1B[48;5;%sm%s\x1B[0m", colorCode, line)
-        }
+		var colorCode string
+		if segment == 1 {
+			colorCode = "197"
+		}
+		if segment == 2 {
+			colorCode = "220"
+		}
+		if segment == 3 {
+			colorCode = "038"
+		}
+		fmt.Printf("\x1B[48;5;%sm%s\x1B[0m", colorCode, line)
+	}
 }
